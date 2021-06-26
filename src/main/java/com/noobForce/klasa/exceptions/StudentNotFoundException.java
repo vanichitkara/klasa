05a5493a03431b3c3d1a8765975e751ddab43ae3
Ojus Singhal <ojus.singhal17@gmail.com>
2021-06-27 00:04:46 +0530

    added exceptions

---
+++ src/main/java/com/noobForce/klasa/exceptions/StudentNotFoundException.java
@@ -0,0 +1,10 @@
+package com.noobForce.klasa.exceptions;
+
+public class StudentNotFoundException extends RuntimeException
+{
+    public StudentNotFoundException(String message)
+    {
+        super(message);
+    }
+    
+}
